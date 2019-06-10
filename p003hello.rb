3a5f136ec941cc67aeb481f6d5acd186aa7a40aa
Stéphane Menanteau <stfm77@gmail.com>
2019-06-10 23:22:52 +0200

    Remove ending space on each lines

--- p003hello.rb
+++ p003hello.rb
@@ -1,22 +1,22 @@
-# p003rubystrings.rb  
-=begin  
-  Ruby Strings  
-  In Ruby, strings are mutable  
-=end  
-  
-puts "Hello World"  
-# Can use " or ' for Strings, but ' is more efficient  
-puts 'Hello World'  
-# String concatenation  
-puts 'I like' + ' Ruby'  
-# Escape sequence  
-puts 'It\'s my Ruby'  
-# New here, displays the string three times  
-puts 'Hello' * 3  
-# Defining a constant  
-# More on Constants later, here  
-# /satishtalim/ruby_names.html  
-PI = 3.1416  
+# p003rubystrings.rb
+=begin
+  Ruby Strings
+  In Ruby, strings are mutable
+=end
+
+puts "Hello World"
+# Can use " or ' for Strings, but ' is more efficient
+puts 'Hello World'
+# String concatenation
+puts 'I like' + ' Ruby'
+# Escape sequence
+puts 'It\'s my Ruby'
+# New here, displays the string three times
+puts 'Hello' * 3
+# Defining a constant
+# More on Constants later, here
+# /satishtalim/ruby_names.html
+PI = 3.1416
 puts PI
 # Test mutable object
 a='This is my test'
